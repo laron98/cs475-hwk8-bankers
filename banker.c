fd93abdc4b866d297fd56124863040476cf7c995
lgover@pugetsound.edu <lwgover@pugetsound.edu>
2023-04-23 22:21:03 +0000

    Doesn't work, good error checking though

--- banker.c
+++ banker.c
@@ -14,32 +14,35 @@ int isDone(int*v,int len){int b=1;for(int i=-1;++i<len;b*=v[i]);return b;}
 void printMat(int**mat,int n,int m){for(int i=-1;++i<n;printVec(mat[i],m));}
 
 // TODO - Safety Algorithm goes here
-int isSafeRecursive(int *available, int **alloc, int **need, int n, int m, char* outputString, int *finish){
+int isSafeRecursive(int *available, int **alloc, int **need, int n, int m, char* outputString, int *finish,int depth){
     if(isDone(finish,n)){
-        printf("SAFE: %s\n",outputString);
+        //printf("SAFE: %s\n",outputString);
         return 1;
     }
-
     int safe = 0;
     int noProgress = 1;
+
+    for(int i =-1;++i<depth*2;printf("  "));
+    printf("available: ");
+    printVec(available,m);
     for(int i=-1;++i<n;){
         if(finish[i]) continue; // if I is finished, ignore
-        // printf("available: ");
-        // printVec(available,m);
-        // printf("need[%u]: ",i);
-        // printVec(need[i],m);
-        if(vectorLessThan(need[i],available,m)){ // all values in (need - alloc) are less than or equal to available
+
+        for(int i =-1;i++<depth*2;printf("  ")); 
+        printf("need[%u]: ",i);
+        printVec(need[i],m);
+        if(vectorLessThanEq(need[i],available,m)){ // all values in (need - alloc) are less than or equal to available
             vectorPlusEquals(available,alloc[i],m);
             finish[i] = 1;
 
             sprintf(outputString,"%sT%u ",outputString,i);
 
-            safe = isSafeRecursive(available,alloc,need,n,m,outputString,finish)||safe;
+            safe = isSafeRecursive(available,alloc,need,n,m,outputString,finish,depth+1)||safe;
             noProgress = 0;
             //undo modifications, so as not to mess up other recursions
             outputString[strlen(outputString)-3] = '\0';
             finish[i] = 0;
-            vectorMinusEquals(available,need[i],m);
+            vectorMinusEquals(available,alloc[i],m);
         }
     }
 
@@ -55,29 +58,15 @@ int isSafeRecursive(int *available, int **alloc, int **need, int n, int m, char*
 int isSafe(int *available, int **alloc, int **maxDemand, int n, int m){
 
     int* work = (int*)malloc(m*sizeof(int)); // m is the number of resources
-    for(int i = -1; ++i < m; work[i]=available[i]); // clones available to work
-    int* finish = (int*)calloc(n, sizeof(int)); // n is number of threads, all are set to 0
+    for(int i = -1;++i<m;work[i]=available[i]); // clones available to work
+    int* finish = (int*)calloc(n,sizeof(int)); // n is number of threads, all are set to 0
     int** need = (int**)malloc(n*sizeof(int*)); // m is the number of resources
     for(int i = -1; ++i<n;){need[i]=(int*)malloc(m*sizeof(int));for(int j=-1;++j<m;need[i][j]=maxDemand[i][j]-alloc[i][j]);} // makes need by subtracting alloc from maxDemand
 
-    //is need defined as all the resources needed, or number of remaining resources?
-    //if it's all the resources needed:
-    //for(int i = -1; ++i<n; vectorMinusEquals(need[i],alloc[i],m));
-
     char* outputString = (char *)calloc((n*3),sizeof(char));
 
-    int safe = isSafeRecursive(work,alloc,need,n,m,outputString,finish);
+    int safe = isSafeRecursive(work,alloc,need,n,m,outputString,finish,0);
     free(outputString); free(work); free(finish); for(int i=-1; ++i<n;free(need[i])); free(need);
 
     return safe;
 }
-//      while (exists unfinished thread i && Need[i] <= Work) {
-//              // pretend that thread i finishes execution
-//              // then OS can reclaim thread i's allocated resources
-//              Work += Alloc[i]
-//              Finish[i] = 1
-//      }
-//      // there's an execution order in which all threads
-//      if (Finish == [1, 1, ..., 1])
-//              return true     // safe!
-//      return false            // unsafe!
