854a938a384bff0e1a0cf6325ba1d2a45c8362fc
lgover@pugetsound.edu <lwgover@pugetsound.edu>
2023-04-24 22:00:50 +0000

    added banker.h

--- banker.h
+++ banker.h
@@ -1,2 +1,6 @@
 /* declarations related to banker's algorithm */
+#define TRUE 1
+#define FALSE 0
+
 int isSafe(int* available, int** alloc, int** need, int n, int m);
+void printMat(int**mat,int n,int m);
